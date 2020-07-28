e355b57c9dbd338864005dc423b156e0deeae3c5
Asrivast1 <askandshankar2001@gmail.com>
2020-07-28 15:10:26 +0530

    Image download code for making datasets through google_images_download package

--- web_scrape.py
+++ web_scrape.py
@@ -95,9 +95,3 @@ data = '''{
 info = json.loads(data) # loads() converts the json file into readable text format
 print('Name:',info["name"]) # info returns the text within the tag
 print('Hide:',info["email"]["hide"]) # Getting into subfolders can be done as follows
-# Downloading images from google search for datasets using python script - Web Scraping Hack
-from google_images_download import google_images_download
-response = google_images_download.googleimagesdownload()
-arguments = {"keywords":"Crops", "limit":9,'print_urls':True}
-paths = response.download(arguments)
-print(paths)
