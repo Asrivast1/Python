1c0d4c5cceb457e68fceae664495166a12fcfcbd
Asrivast1 <askandshankar2001@gmail.com>
2020-08-26 19:45:08 +0530

    Deleted the script

--- web_scraping.py
+++
@@ -1,6 +0,0 @@
-# Downloading images from google search for datasets using python script - Web Scraping Hack
-from google_images_download import google_images_download
-response = google_images_download.googleimagesdownload()
-arguments = {"keywords":"Crops", "limit":9,'print_urls':True}
-paths = response.download(arguments)
-print(paths)
