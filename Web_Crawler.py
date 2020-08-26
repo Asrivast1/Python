e37b5fea9788a0046ad0ec21718ed8ebb68effbf
Asrivast1 <askandshankar2001@gmail.com>
2020-08-26 19:46:43 +0530

    Adding the comments for better understanding.

--- Web_Crawler.py
+++ Web_Crawler.py
@@ -1,12 +1,13 @@
 # Jumping through the links
 import urllib.request, urllib.parse, urllib.error, urllib, ssl
 from bs4 import BeautifulSoup
+# By passing the ssl security patch
 ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
-url = input("Enter the URL:")
-count = int(input('Enter count:'))
-position = int(input('Enter position:'))-1
+url = input("Enter the URL:") # Enter the url to be openend
+count = int(input('Enter count:')) # Number of times the process is to be repeated
+position = int(input('Enter position:'))-1 # Finding the link at the specified position
 html = urllib.urlopen(url, context=ctx).read()
 soup = BeautifulSoup(html,"html.parser")
 href = soup('a')
